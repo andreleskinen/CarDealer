9e90227bf33d3413b94e1cf199697c0ae05f983d
Josef <josefhemme@hotmail.com>
2024-01-29 12:22:33 +0100

    new DTOs

---
+++ CarShop.API.DTO/DTOs/CarColorDTOs.cs
@@ -0,0 +1,14 @@
+﻿namespace CarShop.API.DTO.DTOs;
+
+public class CarColorPostDTO
+{
+    public string Name { get; set; } = string.Empty;
+}
+public class CarColorPutDTO : CarColorPostDTO
+{
+    public int Id { get; set; }
+}
+public class CarColorGetDTO : CarColorPutDTO
+{
+    //public List<FilterGetDTO>? Filters { get; set; }
+}
---
+++ CarShop.API.DTO/DTOs/ColorDTOs.cs
@@ -0,0 +1,14 @@
+﻿namespace CarShop.API.DTO.DTOs;
+
+public class ColorPostDTO
+{
+    public string Name { get; set; } = string.Empty;
+}
+public class ColorPutDTO : ColorPostDTO
+{
+    public int Id { get; set; }
+}
+public class ColorGetDTO : ColorPutDTO
+{
+    //public List<FilterGetDTO>? Filters { get; set; }
+}
