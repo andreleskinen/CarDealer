54e7d33d9a8d175a96a7eb9d421e96b094054388
vonb313 <emil.joakim.berg@hotmail.com>
2024-01-29 13:26:28 +0100

    API Category

--- CarShop.API.Category/Program.cs
+++ CarShop.API.Category/Program.cs
@@ -1,16 +1,20 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// SQL Server Service Registration 
+// SQL Server Service Registration
 builder.Services.AddDbContext<CarShopContext>(
     options =>
         options.UseSqlServer(
             builder.Configuration.GetConnectionString("CarShopConnection")));
 
+/**********
+ ** CORS Cross-Origin Resource Sharing**
+ **********/
 builder.Services.AddCors(policy =>
 {
     policy.AddPolicy("CorsAllAccessPolicy", opt =>
@@ -35,19 +39,22 @@ app.UseHttpsRedirection();
 
 RegisterEndpoints();
 
+/************************
+ ** CORS Configuration **
+ ************************/
 app.UseCors("CorsAllAccessPolicy");
 
 app.Run();
 
 void RegisterEndpoints()
 {
-    //app.AddEndpoint<CarCategory, CarCategoryDTO>();   
-    app.AddEndpoint<Car, CarPostDTO, CarPutDTO, CarGetDTO>();
+    app.AddEndpoint<CarCategory, CarCategoryDTO>();
+    app.AddEndpoint<Category, CategoryPostDTO, CategoryPutDTO, CategoryGetDTO>();
     /*app.MapGet($"/api/categorieswithdata", async (IDbService db) =>
     {
         try
         {
-            return Results.Ok(await ((CarDbService)db).GetCategoriesWithAllRelatedDataAsync());
+            return Results.Ok(await ((CategoryDbService)db).GetCategoriesWithAllRelatedDataAsync());
         }
         catch
         {
@@ -55,27 +62,27 @@ void RegisterEndpoints()
 
         return Results.BadRequest($"Couldn't get the requested products of type {typeof(Product).Name}.");
     });*/
+
 }
+
 void RegisterServices()
 {
     ConfigureAutoMapper();
     builder.Services.AddScoped<IDbService, CategoryDbService>();
 }
+
 void ConfigureAutoMapper()
 {
     var config = new MapperConfiguration(cfg =>
     {
-        cfg.CreateMap<Car, CarPostDTO>().ReverseMap();
-        cfg.CreateMap<Car, CarPutDTO>().ReverseMap();
-        cfg.CreateMap<Car, CarGetDTO>().ReverseMap();
-        cfg.CreateMap<CarCategory, CarCategoryDTO>().ReverseMap();
-        /*
-        cfg.CreateMap<Filter, FilterGetDTO>().ReverseMap();
+        cfg.CreateMap<Category, CategoryPostDTO>().ReverseMap();
+        cfg.CreateMap<Category, CategoryPutDTO>().ReverseMap();
+        cfg.CreateMap<Category, CategoryGetDTO>().ReverseMap();
+        cfg.CreateMap<Category, CategorySmallGetDTO>().ReverseMap();
+        /*cfg.CreateMap<Filter, FilterGetDTO>().ReverseMap();
         cfg.CreateMap<Size, OptionDTO>().ReverseMap();
-        cfg.CreateMap<Color, OptionDTO>().ReverseMap();
-        */
+        cfg.CreateMap<Color, OptionDTO>().ReverseMap();*/
     });
     var mapper = config.CreateMapper();
     builder.Services.AddSingleton(mapper);
 }
-
