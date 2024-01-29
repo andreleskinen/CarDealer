c5e55f3d98838af16ec31a438e7451279d1feeb0
André Leskinen <andre.leskinen@gmail.com>
2024-01-29 12:29:18 +0100

    added dtos for entities

---
+++ CarShop.API.DTO/DTOs/MileageDTOs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShop.API.DTO;
+
+public class MileagePostDTO
+{
+    public string Name { get; set; } = string.Empty;
+}
+public class MileagePutDTO : MileagePostDTO
+{
+    public int Id { get; set; }
+}
+public class MileageGetDTO : MileagePutDTO
+{
+    //public List<FilterGetDTO>? Filters { get; set; }
+}
---
+++ CarShop.API.DTO/DTOs/ModelDTOs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShop.API.DTO;
+
+public class ModelPostDTO
+{
+    public string Name { get; set; } = string.Empty;
+}
+public class ModelPutDTO : ModelPostDTO
+{
+    public int Id { get; set; }
+}
+public class ModelGetDTO : ModelPutDTO
+{
+    //public List<FilterGetDTO>? Filters { get; set; }
+}
