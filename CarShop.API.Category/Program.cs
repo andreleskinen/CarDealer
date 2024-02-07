bc03c27e77daca75cb0458e2270979f395ac8238
vonb313 <emil.joakim.berg@hotmail.com>
2024-02-07 18:28:11 +0100

    Logo,navigationmenu work in progress

--- CarShop.API.Category/Program.cs
+++ CarShop.API.Category/Program.cs
@@ -83,7 +83,7 @@ void ConfigureAutoMapper()
         cfg.CreateMap<Category, CategoryPutDTO>().ReverseMap();
         cfg.CreateMap<Category, CategoryGetDTO>().ReverseMap();
         cfg.CreateMap<Category, CategorySmallGetDTO>().ReverseMap();
-        
+
 
         //cfg.CreateMap<CarCategory, CarCategoryDTO>().ReverseMap();
         /*
@@ -91,7 +91,7 @@ void ConfigureAutoMapper()
         cfg.CreateMap<Size, OptionDTO>().ReverseMap();
         cfg.CreateMap<Color, OptionDTO>().ReverseMap();
         */
-    })
+    });
     
     var mapper = config.CreateMapper();
     builder.Services.AddSingleton(mapper);
